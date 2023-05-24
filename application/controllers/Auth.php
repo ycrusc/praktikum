41714204fc2c911dfc18ee254eaa88fea6bcf5f6
Yohanes Crusc <ycrusc@gmail.com>
2023-05-24 20:28:21 +0700

    add dashboard

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,30 +1,123 @@
 <?php
 
+use PSpell\Config;
+
 class Auth extends CI_Controller
 {
 
    public function __construct()
    {
       parent::__construct();
-      $this->load->model('Wisata');
+      $this->load->library('form_validation');
+      $this->load->model('UserModel');
+      $this->load->library('session');
    }
-
 
    public function index()
    {
-      $data['judul'] = 'Login';
-      $data['wisata'] = $this->Wisata->getAllWisata();
+      $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+      $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
-      $this->load->view('auth/headerAuth', $data);
-      $this->load->view('auth/login');
-      $this->load->view('auth/footerAuth');
+      if ($this->form_validation->run() == false) {
+         $data['judul'] = 'Login';
+
+         $this->load->view('auth/login', $data);
+      } else {
+
+         $this->_login();
+      }
+   }
+
+   private function _login()
+   {
+      $email = $this->input->post('email');
+      $password = $this->input->post('password');
+
+      $user = $this->db->get_where('USERS', ['E_MAIL' => $email])->row_array();
+      // var_dump($user);
+      // die;
+      if ($user) {
+         if ($user['IS_ACTIVE'] == 1) {
+            if (password_verify($password, $user['PASSWORD'])) {
+               $data = [
+                  'email' => $user['E_MAIL'],
+                  'role_id' => $user['ROLE_ID']
+               ];
+               $this->session->set_userdata($data);
+               redirect('user');
+            } else {
+               $this->session->set_flashdata('message', '<div class="alert alert-danger" 
+                     role="alert"> Password Salah</div>');
+               redirect('auth');
+            }
+         } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" 
+            role="alert"> Email Tidak aktif</div>');
+            redirect('auth');
+         }
+      } else {
+         $this->session->set_flashdata('message', '<div class="alert alert-danger" 
+         role="alert"> Email Tidak ada</div>');
+         redirect('auth');
+      }
    }
 
 
    public function registration()
    {
-      $data['judul'] = 'Registration';
 
-      $this->load->view('auth/registration');
+      $config['total_users'] = $this->UserModel->getCountAllUsers();
+
+      $this->form_validation->set_rules('full_name', 'Name', 'required|trim');
+      $this->form_validation->set_rules('user_name', 'userName', 'required|trim|is_unique[USERS.USER_NAME]', [
+         'is_unique' => 'This username has already exist'
+      ]);
+      $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[USERS.E_MAIL]', [
+         'is_unique' => 'This email has already registred'
+      ]);
+      $this->form_validation->set_rules('phone', 'Phone', 'required|regex_match[/^[0-9]{10,12}$/]');
+      $this->form_validation->set_rules(
+         'password1',
+         'Password',
+         'required|trim|min_length[6]|matches[password2]',
+         [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password to short!'
+         ]
+      );
+      $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+      if ($this->form_validation->run() == false) {
+         $data['judul'] = 'Registration';
+         $this->load->view('auth/registration', $data);
+      } else {
+         $date = date_create();
+         $now = date_format($date, "Y-m-d H:i:s");
+         $timestamp = strtotime($now);
+         $data = [
+            'ID' => $config['total_users'],
+            'PASSWORD' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+            'USER_NAME' => htmlspecialchars($this->input->post('user_name', true)),
+            'FULL_NAME' => htmlspecialchars($this->input->post('full_name', true)),
+            'PHONE_NUMBER' => $this->input->post('phone'),
+            'E_MAIL' => htmlspecialchars($this->input->post('email', true)),
+            'IS_ACTIVE' => 1,
+            'ROLE_ID' => 4,
+         ];
+         $this->db->insert('USERS', $data);
+         $this->session->set_flashdata('message', '<div class="alert alert-success" 
+         role="alert"> Data Berhasil Ditambah. Silahkan Login</div>');
+         redirect('auth');
+      }
+   }
+
+   public function logout()
+   {
+      $this->session->unset_userdata('email');
+      $this->session->unset_userdata('role_id');
+
+      $this->session->set_flashdata('message', '<div class="alert alert-success" 
+      role="alert">Anda Berhasil Logout</div>');
+      redirect('auth');
    }
 }
